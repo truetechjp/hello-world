c7f52efa98c1cfc268d993fd6b6bc4dd9b2304c6
Makoto TSUCHIYA <makoto.tsuchiya@truetech.jp>
2016-04-28 01:24:50 +0900

    メッセージ文字列が未指定の場合のメッセージ表示

--- HelloWorld/src/jp/truetech/hello/Main.java
+++ HelloWorld/src/jp/truetech/hello/Main.java
@@ -3,6 +3,9 @@ package jp.truetech.hello;
 public class Main {
 
 	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("no message");
+		}
 		if (args.length > 0) {
 			new Greeting().say(args[0]);
 		}
