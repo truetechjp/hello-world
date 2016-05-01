6dc19f384b2de4a115a152e8f6ce949d81392625
Makoto TSUCHIYA <makoto.tsuchiya@trutech.jp>
2016-05-02 04:52:24 +0900

    プロジェクトのプロパティを変更
    MS932→UTF8

--- HelloWorld/src/jp/truetech/hello/Main.java
+++ HelloWorld/src/jp/truetech/hello/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		if (args.length < 1) {
-			System.out.println("no message");
+			System.out.println("no message (メッセージを指定してください");
 		}
 		if (args.length > 0) {
 			new Greeting().say(args[0]);
